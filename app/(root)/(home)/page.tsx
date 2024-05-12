170882f4d6d2fdd924c9a2b77353f30c3f3cac5e
Priyanshu2957 <138032954+Priyanshu2957@users.noreply.github.com>
2024-05-12 12:53:29 +0530

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -6,7 +6,7 @@ const ds = Sedgwick_Ave_Display({
   subsets : ['latin'],
 })      
 const Home = () => {
-  const timeNow = new Date().getHours();
+  const curr = new Date();
   const time = curr.toLocaleTimeString('en-US',{hour:'2-digit',minute:'2-digit',timeZone: 'Asia/Kolkata'});
   const date = curr.toLocaleDateString('en-US',{dateStyle:'full',timeZone: 'Asia/Kolkata'});
   const greeting = time >= '5' && time < '12'
@@ -18,7 +18,7 @@ const Home = () => {
     <section className='flex size-full flex-col gap-10 text-white'>
       <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
         <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 md:p-9 lg:p-11'>
-          <h1 className='glassmorphism max-w-[200px] rounded py-2 text-center text-xl font-normal'><span className={ds.className}>{greeting}</span></h1>
+          <h1 className='glassmorphism max-w-[170px] rounded py-2 text-center text-xl font-normal'><span className={ds.className}>{greeting}</span></h1>
           <div className='flex flex-col gap-2'>
             <h1 className='text-4xl font-extrabold md:text-5xl lg:text-7xl'>
                 {time}
